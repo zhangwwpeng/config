6809fc45da34cfd602e9b71a8359ccab47f0451c
zhangwp <zhangwwpeng@gmail.com>
2025-09-24 18:40:51 +0800

    change: remove lazy.nvim to self config

--- lua/plugins/disable.lua
+++
@@ -1,6 +0,0 @@
-return {
-	{ "tokyonight.nvim", enabled = false },
-	{ "folke/flash.nvim", enabled = false },
-	{ "nvim-treesitter/nvim-treesitter-context", enabled = true },
-	{ "stevearc/aerial.nvim", enabled = true },
-}
--- lua/plugins/venn.lua
+++
@@ -1,6 +0,0 @@
-return {
-	{
-		"jbyuki/venn.nvim",
-		event = "VeryLazy",
-	},
-}
